3063760b42800bd017e978c76233fc2f3dd19c03
Bulat Mazitov <mazikazik65@gmail.com>
2020-07-02 20:16:59 +0400

    End ?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,12 @@ import somepackage.Some;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        Some some = new Some("Это уже не секрет");
+        System.out.println(some.toString());
+        System.out.println("some.count() = " + some.count());
+        System.out.println("some.getSecret() = " + some.getSecret());
+        some.setNoMoreSecrets(some.getДаТакТожеМожно());
+        System.out.println(some.getSecret( true));
     }
 }
